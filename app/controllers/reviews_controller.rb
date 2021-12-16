08c1fb4cc19daae48b3f106c3fcb740de506962c
Betty <betty_elfagid@yahoo.co.uk>
2021-12-16 01:55:07 +0000

    custom book create route added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,7 @@ class ReviewsController < ApplicationController
 
  
   def create
+   
     @review = Review.new(review_params)
     if @review.save
       render json: @review, status: :created, location: @review
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+ 
   resources :books do
-    resources :reviews
+    resources :reviews, only: [:index, :show]
   end
+
+  post '/books/:book_id/reviews', to: 'reviews#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
